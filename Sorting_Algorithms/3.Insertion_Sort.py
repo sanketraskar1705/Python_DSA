f7fd810c1361da399215865664f222c15650f70a
SanketR-1705 <155961251+sanketraskar1705@users.noreply.github.com>
2026-01-15 01:13:00 +0530

    Insertion Sort

--- Sorting_Algorithms/3.Insertion_Sort.py
+++ Sorting_Algorithms/3.Insertion_Sort.py
@@ -9,7 +9,7 @@ class Solution:
             key = arr[i]
             j = i -1
 
-            while j >= 0 and key < arr[j]:
+            while j >= 0 and arr[j] > key:
                 arr[j+1] = arr[j]
                 j -= 1
 
